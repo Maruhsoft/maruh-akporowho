31d1372b743181d46cd849a60c6cb1772defaf65
Maruh Phronesis Vulcan Akporowho <59612588+Maruhsoft@users.noreply.github.com>
2024-03-28 17:04:50 +0100

    Add files via upload
    Excluding the node modules

---
+++ src/constants/index.js
@@ -0,0 +1,177 @@
+import {
+  frontend,
+  backend,
+  cybersecurity,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'Cyber Security',
+    icon: cybersecurity,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Software Tester',
+    company_name: 'DukaniHub LLC',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Jul 2022 - Oct 2023',
+  },
+  {
+    title: 'Head of IT',
+    company_name: 'GOTNI Leadership Centre',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Feb 2022 - Sep 2023',
+  },
+  {
+    title: 'Mentor on Cyber Security (Volunteer)',
+    company_name: 'Empower Her Community',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: 'April 2023',
+  },
+  {
+    title: 'Remote Developer',
+    company_name: 'GitIT Technology Limited',
+    icon: dcc,
+    iconBg: '#333333',
+    date: 'Feb 2018 - Mar 2018',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'KomiKult',
+    description: 'A comic characters list app that displays Marvel characters.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/Maruhsoft/komikolt',
+    demo: 'https://komikult.netlify.app/',
+  },
+];
+
+export { services, technologies, experiences, projects };
